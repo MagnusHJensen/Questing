194f6598a458f34e17cc122545047a8060adc3cf
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-09-02 14:08:46 +0200

    added mobzone map, and created a static reference to plugin, also added new executors and listeners

--- src/main/java/dk/magnusjensen/questing/Questing.java
+++ src/main/java/dk/magnusjensen/questing/Questing.java
@@ -2,11 +2,13 @@ package dk.magnusjensen.questing;
 
 import dk.magnusjensen.questing.commands.*;
 import dk.magnusjensen.questing.data.Quest;
-import dk.magnusjensen.questing.listeners.OnPlayerJoin;
-import dk.magnusjensen.questing.listeners.QuestNPCInteract;
-import dk.magnusjensen.questing.listeners.UUIDCheckListener;
+import dk.magnusjensen.questing.data.zones.MobZone;
+import dk.magnusjensen.questing.listeners.*;
+import dk.magnusjensen.questing.util.MobZoneIO;
 import dk.magnusjensen.questing.util.PlayerIO;
 import dk.magnusjensen.questing.util.QuestParser;
+import org.bukkit.Bukkit;
+import org.bukkit.conversations.ConversationFactory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -16,26 +18,33 @@ import java.util.UUID;
 
 public final class Questing extends JavaPlugin {
 
+	public static Questing INSTANCE;
 	public static final String DATA_PATH = "plugins/questing/";
 
 	public static HashMap<UUID, ArrayList<Quest>> quests = new HashMap<>();
 	public static HashMap<UUID, HashMap<String, ArrayList<String>>> players = new HashMap<>();
+	public static ArrayList<MobZone> mobZones = new ArrayList<>();
+	public static ConversationFactory conversationFactory = new ConversationFactory(Bukkit.getPluginManager().getPlugin("questing"));
 
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
+		INSTANCE = this;
+
 		this.getCommand("uuidcheck").setExecutor(new UUIDCheck());
 		this.getCommand("abandon").setExecutor(new Abandon());
 		this.getCommand("start").setExecutor(new Start());
 		this.getCommand("deny").setExecutor(new Deny());
 		this.getCommand("save").setExecutor(new Save());
-
-
+		this.getCommand("mobzone").setExecutor(new CreateMobZone());
 
 		getServer().getPluginManager().registerEvents(new UUIDCheckListener(), this);
 		getServer().getPluginManager().registerEvents(new QuestNPCInteract(), this);
 		getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
+		getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
+		getServer().getPluginManager().registerEvents(new DisablingDamageQuestgivers(), this);
 
+		mobZones = MobZoneIO.startParsingMobZones();
 		quests = QuestParser.startParsingQuests();
 		players = PlayerIO.startParsingPlayers();
 
@@ -54,5 +63,13 @@ public final class Questing extends JavaPlugin {
 		// Plugin shutdown logic
 
 		PlayerIO.savePlayers(players);
+
+		for (MobZone zone : mobZones) {
+			zone.cleanUp(); // Clean up mobs on disable.
+		}
+
+		MobZoneIO.saveMobZones(mobZones);
 	}
+
+
 }
