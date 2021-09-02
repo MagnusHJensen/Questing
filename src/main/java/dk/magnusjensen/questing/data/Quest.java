74a823da3c0ee81975af9905bc3feaed17de75bc
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-09-02 13:58:41 +0200

    check for start req. remove PDC data if quest is abandoned and give quest completion more feel

--- src/main/java/dk/magnusjensen/questing/data/Quest.java
+++ src/main/java/dk/magnusjensen/questing/data/Quest.java
@@ -4,8 +4,23 @@ import dk.magnusjensen.questing.Questing;
 import dk.magnusjensen.questing.data.requirements.completion.IQuestCompletion;
 import dk.magnusjensen.questing.data.requirements.start.IQuestStart;
 import dk.magnusjensen.questing.data.rewards.IQuestReward;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.title.Title;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
 
@@ -38,10 +53,14 @@ public class Quest {
 	public void interact(Player player) {
 
 
+		if (!this.startReq.canStartQuest(player) && !this.ongoingBy.contains(player.getUniqueId())) {
+			this.paragraphs.get("requirements_not_met").play(this, player);
+			return;
+		}
 		// Have the user accepted the quest, and have not finished the requirements to complete it.
-		if (this.ongoingBy.contains(player.getUniqueId()) && !this.completionReq.canCompleteQuest(player)) {
+		if (this.ongoingBy.contains(player.getUniqueId()) && !this.completionReq.canCompleteQuest(this, player)) {
 			this.paragraphs.get("ongoing").playWithAbandon(this, player, ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/questing:abandon " + this.questOwner.toString() + "/" + this.id));
-		} else if (this.ongoingBy.contains(player.getUniqueId()) && this.completionReq.canCompleteQuest(player)){
+		} else if (this.ongoingBy.contains(player.getUniqueId()) && this.completionReq.canCompleteQuest(this, player)){
 			// Ongoing and can complete
 			this.paragraphs.get("completion").play(this, player);
 			this.completeQuest(player);
@@ -64,6 +83,9 @@ public class Quest {
 
 	public void abandonQuest(Player player) {
 		this.paragraphs.get("abandon").play(this, player);
+		if (this.getCompletionReq().hasMobKills()) {
+			player.getPersistentDataContainer().set(new NamespacedKey(Questing.INSTANCE, this.getCustomQuestId()), PersistentDataType.INTEGER, 0);
+		}
 		this.removePlayerFromOngoing(player);
 	}
 
@@ -78,13 +100,50 @@ public class Quest {
 		this.completedBy.add(player.getUniqueId());
 		Questing.players.get(player.getUniqueId()).get("completed").add(this.questOwner + "/" + this.id);
 		this.removePlayerFromOngoing(player);
+
+		final Component mainTitle = Component.text("Quest Completed" , NamedTextColor.GREEN);
+		final Component subTitle = Component.text(this.name, NamedTextColor.GRAY);
+		final Title title = Title.title(mainTitle, subTitle);
+
+		Audience.audience(player).showTitle(title);
+
+		// Set up firework, and start firing off.
+		Firework fw = player.getWorld().spawn(player.getLocation(), Firework.class);
+
+		FireworkMeta fwm = fw.getFireworkMeta();
+		fwm.addEffect(FireworkEffect.builder().withFade(Color.AQUA).with(FireworkEffect.Type.CREEPER).withColor(Color.GREEN, Color.RED).build());
+		fwm.setPower(1);
+
+		fw.setFireworkMeta(fwm);
+
+		new BukkitRunnable() {
+			int i = 0;
+			@Override
+			public void run() {
+				Firework fw1 = player.getWorld().spawn(player.getLocation(), Firework.class);
+				fw1.setFireworkMeta(fwm);
+				i++;
+				if (i == 2) {
+					this.cancel();
+				}
+			}
+		}.runTaskTimer(Bukkit.getPluginManager().getPlugin("questing"), 5, 15);
+	}
+
+	public String getCustomQuestId() {
+		return new StringBuilder().append(questOwner.toString()).append("/").append(id).toString();
+	}
+
+	public boolean requiresMobKills() {
+		return completionReq.hasMobKills();
+	}
+
+	public IQuestCompletion getCompletionReq() {
+		return completionReq;
 	}
 
 
-
-
-
-
-
-
+	public String getName() {
+		return name;
+	}
 }
