74a77316e10d541d7e0555399447ae761ce84c27
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-09-02 14:03:06 +0200

    added slayer completion type parser

--- src/main/java/dk/magnusjensen/questing/util/QuestCompletionParser.java
+++ src/main/java/dk/magnusjensen/questing/util/QuestCompletionParser.java
@@ -1,11 +1,17 @@
 package dk.magnusjensen.questing.util;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import dk.magnusjensen.questing.Questing;
 import dk.magnusjensen.questing.data.requirements.completion.IQuestCompletion;
 import dk.magnusjensen.questing.data.requirements.completion.ItemCompletion;
+import dk.magnusjensen.questing.data.requirements.completion.MobCompletion;
+import dk.magnusjensen.questing.data.requirements.completion.NoneCompletion;
 import dk.magnusjensen.questing.data.requirements.start.IQuestStart;
 import dk.magnusjensen.questing.data.requirements.start.NoneStart;
 import dk.magnusjensen.questing.data.rewards.ItemReward;
+import dk.magnusjensen.questing.data.zones.MobZone;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -14,12 +20,14 @@ import java.util.ArrayList;
 
 public class QuestCompletionParser {
 
-	protected static IQuestCompletion parseQuestCompletion(JsonObject completionObject) {
+	protected static IQuestCompletion parseQuestCompletion(String questid, JsonObject completionObject) {
 		if (completionObject.get("type").getAsString().equalsIgnoreCase("item")) {
 			return parseItemCompeletion(completionObject);
+		} else if (completionObject.get("type").getAsString().equalsIgnoreCase("slayer")) {
+			return parseSlayerCompletion(questid, completionObject);
+		} else {
+			return new NoneCompletion();
 		}
-		System.out.println("Quest Start type not known!");
-		return null;
 	}
 
 	private static ItemCompletion parseItemCompeletion(JsonObject completionObject) {
@@ -44,6 +52,27 @@ public class QuestCompletionParser {
 		ArrayList<ItemStack> stacks = new ArrayList<>();
 		stacks.add(stack);
 		return new ItemCompletion(stacks, take);
+	}
+
+	private static MobCompletion parseSlayerCompletion(String questid, JsonObject completionObject) {
+		JsonObject data = completionObject.getAsJsonObject("data");
+		JsonArray zoneIds = data.get("zoneIds").getAsJsonArray();
+
+		ArrayList<MobZone> zones = new ArrayList<>();
+		for (JsonElement zoneIdEle : zoneIds) {
+			int zoneId = zoneIdEle.getAsInt();
+			if (Questing.mobZones.size() < zoneId) {
+				continue;
+			}
+			MobZone zone = Questing.mobZones.get(zoneId - 1);
+			if (zone != null) {
+				zones.add(zone);
+				zone.linkQuest(questid);
+			}
+		}
+		int amountToKill = data.get("amount").getAsInt();
+
+		return new MobCompletion(amountToKill, zones);
 	}
 
 }
